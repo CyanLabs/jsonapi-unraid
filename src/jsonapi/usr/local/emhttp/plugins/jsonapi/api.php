f8dfddff013e37bad80cddee7afa88c895ba8593
Jiminald <code@jiminald.co.uk>
2019-12-29 21:09:07 +0000

    Show list of files if no file has been requested, or if the file requested does not exist

--- src/jsonapi/usr/local/emhttp/plugins/jsonapi/api.php
+++ src/jsonapi/usr/local/emhttp/plugins/jsonapi/api.php
@@ -2,8 +2,15 @@
 $files = glob("/var/local/emhttp/*.ini");
 $file = htmlspecialchars($_GET["file"]);
 if (in_array("/var/local/emhttp/" . $file, $files))  {
-$output = parse_ini_file("/var/local/emhttp/" . $file, true);
-header('Content-Type: application/json');
-echo json_encode($output);
+    $output = parse_ini_file("/var/local/emhttp/" . $file, true);
+    header('Content-Type: application/json');
+    echo json_encode($output);
+} else {
+    echo '<ul>';
+    foreach ($files as $file) {
+        $file = basename($file);
+        echo '<li><a href="?file='.$file.'">'.$file.'</a></li>';
+    } // End of foreach "Looping ini files"
+    echo '</ul>';
 }
 ?>
